0c5d8923342669536afd00c9e4a593b7695c630a
o.ghodrati <o.ghodrati@rayanbourse.ir>
2023-09-11 13:27:41 +0330

    ExtensionMethod

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExtensionMethod;
+using System.Runtime.CompilerServices;
+
+//await Do();
+//Console.WriteLine(await DoWorkAsync());
+//Console.WriteLine( DoWork());
+
+await Task.Run(() => {Thread.Sleep(5000);});
+Console.WriteLine("level 1");
+await Task.Run(() => { Thread.Sleep(10000); });
+Console.WriteLine("level 2");
+await Task.Run(() => { Thread.Sleep(5000); });
+Console.WriteLine("level 3");
+string DoWork()
+{
+  
+  Thread.Sleep(10000);
+    return "done";
+}
+
+Task<string> DoWorkAsync()
+{
+    return Task.Run(() =>
+    {
+        Thread.Sleep(10000);
+         return "hello async";
+    });
+}
+
+ async Task Do()
+{
+    await Task.Run(() =>
+    {
+        Thread.Sleep(5000);
+        
+    });
+}
+
+
---
+++ ExtensionMethod/myExtensionMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExtensionMethod
+{
+    public static class myExtensionMethod
+    {
+        public static string toShamsi(this DateTime value)
+        {
+            PersianCalendar pc=new PersianCalendar();
+            return pc.GetYear(value).ToString() +"/"+ pc.GetMonth(value).ToString("00")+"/"+pc.GetDayOfMonth(value).ToString();
+          
+        }
+    }
+}
